f446496c1163bc5d4a2338b9470b835dd9c65d7d
AnJeongMin1 <dlstod134@naver.com>
2025-03-25 16:03:37 +0900

    Item

--- Assets/Script/EvolutionItem.cs
+++ Assets/Script/EvolutionItem.cs
@@ -5,7 +5,14 @@ public class EvolutionItem : MonoBehaviour
     public float ItemVelocity = 2f;
     public float UpwardForce = 5f;
     public float RotationSpeed = 300f;
-    Rigidbody2D rig = null;
+
+    public float floatSpeed = 1.5f;
+    public float floatAmount = 0.1f;
+    Rigidbody2D rig;
+    private bool hasLanded = false;
+    private Vector3 startPos;
+
+
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
@@ -13,12 +20,29 @@ public class EvolutionItem : MonoBehaviour
 
         rig.angularVelocity = RotationSpeed;
     }
- 
+
     void Update()
     {
-
+        if (hasLanded)
+        {
+            
+            transform.position = startPos + new Vector3(0, Mathf.Sin(Time.time * floatSpeed) * floatAmount, 0);
+        }
     }
-    //∫∏Ω∫¬ ¿∏∑Œ ¿Ãµø«œ∏È µ…µÌ
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!hasLanded)
+        {
+            hasLanded = true;
+            startPos = transform.position; 
+            rig.linearVelocity = Vector2.zero;
+            rig.angularVelocity = 0f;
+            rig.bodyType = RigidbodyType2D.Kinematic;
+        }
+    }
+
+    //Î≥¥Ïä§Ï™ΩÏóêÏÑú ÎìúÎûçÏãúÌÇ§Í∏∞
     //public void EvolutionDrop()
     //{
     //    Instantiate(EvolutionItem, transform.position, Quaternion.identity);
--- Assets/Script/GravityPowerUp.cs
+++ Assets/Script/GravityPowerUp.cs
@@ -5,8 +5,12 @@ public class GravityPowerUp : MonoBehaviour
     public float ItemVelocity = 2f;
     public float UpwardForce = 5f;
     public float RotationSpeed = 350f;
-    
+
+    public float floatSpeed = 1f;
+    public float floatAmount = 0.02f; 
     Rigidbody2D rig;
+    private bool hasLanded = false;
+    private Vector3 startPos;
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
@@ -17,7 +21,23 @@ public class GravityPowerUp : MonoBehaviour
 
     void Update()
     {
+        if (hasLanded)
+        {
+            
+            transform.position = startPos + new Vector3(0, Mathf.Sin(Time.time * floatSpeed) * floatAmount, 0);
+        }
+    }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!hasLanded)
+        {
+            hasLanded = true;
+            startPos = transform.position; 
+            rig.linearVelocity = Vector2.zero;
+            rig.angularVelocity = 0f;
+            rig.bodyType = RigidbodyType2D.Kinematic; 
+        }
     }
 
 }
